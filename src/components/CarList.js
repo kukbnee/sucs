6ba66b0ea93757d2beea0c624119843d9b9988d6
Im Kukbeen <kukbnee@gmail.com>
2023-07-20 10:42:06 +0900

    이미지 슬라이드 연습

--- src/components/CarList.js
+++ src/components/CarList.js
@@ -1,29 +1,29 @@
-import { Button } from "primereact/button";
-import { Card } from "primereact/card";
+import { useState } from 'react';
+import './../assets/style/carList.css';
 
 function CarList() {
-  const header = (
-    <>
-    <img alt="Card" src="/image/Support.ico" />
-    <img alt="Card" src="/image/Warranty.ico" />
-    </>
-  );
-  const footer = (
-      <div className="flex flex-wrap justify-content-end gap-2">
-          <Button label="Save" icon="pi pi-check" />
-          <Button label="Cancel" icon="pi pi-times" className="p-button-outlined p-button-secondary" />
-      </div>
-  );
 
+  let [transState, setTransState] = useState({});
+  const slideImg = (e)=> {
+    let modify = -(e.target.value-1)*100;
+    setTransState({transform: `translate(${modify}vw)`});
+  }
   return (
-      <div className="card flex justify-content-center">
-          <Card title="Title" subTitle="Subtitle" footer={footer} header={header} className="md:w-25rem">
-              <p className="m-0">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae 
-                  numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque quas!
-              </p>
-          </Card>
+    <>
+    <div style={{overflow: "hidden"}}>
+      <div className="container-car-list" style={transState}>
+        <div className="item"><img className="slider" src="/image/임하윤_1.JPG"></img></div>
+        <div className="item"><img className="slider" src="/image/임하윤_2.JPG"></img></div>
+        <div className="item"><img className="slider" src="/image/임하윤_3.JPG"></img></div>
       </div>
+    </div>
+
+    <div className="slide-btn">
+      <button className="slide btn1" onClick={slideImg} value={1}>1</button>
+      <button className="slide btn2" onClick={slideImg} value={2}>2</button>
+      <button className="slide btn3" onClick={slideImg} value={3}>3</button>
+    </div>
+    </>
   )
 }
 
