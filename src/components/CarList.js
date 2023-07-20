2cb531b0e2ed84d4711c8303b7683b2d0beec539
kukbni <kukbnee@gmail.com>
2023-07-20 21:14:42 +0900

    이미지 리스트 화면 퍼블리싱 진행중

--- src/components/CarList.js
+++ src/components/CarList.js
@@ -10,18 +10,29 @@ function CarList() {
   }
   return (
     <>
-    <div style={{overflow: "hidden"}}>
-      <div className="container-car-list" style={transState}>
-        <div className="item"><img className="slider" src="/image/임하윤_1.JPG"></img></div>
-        <div className="item"><img className="slider" src="/image/임하윤_2.JPG"></img></div>
-        <div className="item"><img className="slider" src="/image/임하윤_3.JPG"></img></div>
-      </div>
-    </div>
+    <div className="container-car-list">
+      <ul className="car-list">
+        <li className="car-item">
+          <div className="car-item-img" style={transState}>
+            <div className="item-img"><img className="slider" src="/image/임하윤_1.JPG"></img></div>
+            <div className="item-img"><img className="slider" src="/image/임하윤_2.JPG"></img></div>
+            <div className="item-img"><img className="slider" src="/image/임하윤_3.JPG"></img></div>
+          </div>
 
+          <div className="hot-mark">
+            <span>hotmark1</span>
+            <span>hotmark2</span>
+          </div>
+          <div className="title">모델명 + 등급</div>
+          <div className="sub-title">연식 주행거리 지역 + 가격</div>
+        </li>
+      </ul>
+    </div>
     <div className="slide-btn">
       <button className="slide btn1" onClick={slideImg} value={1}>1</button>
       <button className="slide btn2" onClick={slideImg} value={2}>2</button>
       <button className="slide btn3" onClick={slideImg} value={3}>3</button>
+      <button className="good btn1"></button>
     </div>
     </>
   )
